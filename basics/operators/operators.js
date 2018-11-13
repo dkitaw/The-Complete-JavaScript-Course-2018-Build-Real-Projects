4c830fb2d498c763492f94c34d9ac4199d7300db
robin <kartikrobin@gmail.com>
2018-11-13 17:48:31 +0530

    Learned about math operators

---
+++ basics/operators/operators.js
@@ -0,0 +1,9 @@
+var num1 = 2;
+var num2 = 10;
+
+// Basic math operators
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
