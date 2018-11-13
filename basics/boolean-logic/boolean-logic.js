59627ce3e36c9e19909b67ec0a7b6fceb5d3a7c7
robin <kartikrobin@gmail.com>
2018-11-13 20:01:33 +0530

    Learned about Logical Operators

---
+++ basics/boolean-logic/boolean-logic.js
@@ -0,0 +1,10 @@
+var firstName = 'John';
+var age = 16;
+
+if (age < 13) {
+    console.log(`${firstName} is a boy.`);
+} else if (age >= 13 && age < 20) {
+    console.log(`${firstName} is a teenager.`);
+} else {
+    console.log(`${firstName} is a man.`);
+}
