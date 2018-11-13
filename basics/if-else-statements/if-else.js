e0e0801d43f08801032208c6f8bc086fc6dc166e
robin <kartikrobin@gmail.com>
2018-11-13 19:33:22 +0530

    Learned about If-else statement

---
+++ basics/if-else-statements/if-else.js
@@ -0,0 +1,16 @@
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(`${firstName} is married.`);
+} else {
+    console.log(`${firstName} will marry soon.`);
+}
+
+var isMarried = false;
+
+if (isMarried) {
+    console.log(`${firstName} is already married.`);
+} else {
+    console.log(`${firstName} will hopefully marry soon.`);
+}
