2bb6a66c3f210d202d3fb584e78db79fb6b77e85
robin <kartikrobin@gmail.com>
2018-11-13 17:24:36 +0530

    Learned about Variable Mutation and Type Coercion

---
+++ basics/variable-mutation-and-type-coercion/variable-mutation-type-coercion.js
@@ -0,0 +1,12 @@
+var firstName = 'John';
+var age = 28;
+
+console.log(typeof (firstName + ' ' + age));
+
+var job, isMarried;
+
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + ' .Is he married? ' + isMarried);
+console.log(typeof(firstName + ' is a ' + age + ' years old ' + job + ' .Is he married? ' + isMarried));
