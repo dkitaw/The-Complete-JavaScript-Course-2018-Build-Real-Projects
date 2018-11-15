969ecce5868ca3c625fb06ef6cbc3c22b8b5d9ac
robin <kartikrobin@gmail.com>
2018-11-15 15:17:08 +0530

    Learned how to add, remove and toggle CSS Classes, and Updated scores and changed the active player

--- JS- in-the-browser-DOM-manipulation-and-events/starter/app.js
+++ JS- in-the-browser-DOM-manipulation-and-events/starter/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -32,6 +32,23 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the roundScore but only If the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
     
 
 });
